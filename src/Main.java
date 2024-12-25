157433724add8d0caecc6ddccb2d0b8d0ff79f1f
aditya <adityapai64@gmail.com>
2024-12-25 14:18:12 +0100

    Palindrome check

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         RecursionWithStrings recursionWithStrings = new RecursionWithStrings();
+
         System.out.println(recursionWithStrings.reverseStringMySolution("abcdefg"));
         System.out.println(recursionWithStrings.reverseStringAuthorsSolution("abcdefg"));
-
+        System.out.println(recursionWithStrings.palindromeCheckMySolution("malayalam"));
+        System.out.println(recursionWithStrings.palindromeCheckAuthorsSolution("kayak"));
     }
 }
--- src/RecursionWithStrings.java
+++ src/RecursionWithStrings.java
@@ -20,4 +20,32 @@ public class RecursionWithStrings {
         //concatenating the first char to the result of recursion
         return reverseStringAuthorsSolution(input.substring(1)) + input.charAt(0);
     }
+
+    /*Palindrome: Word which spells the same forwards and backwards*/
+    public boolean palindromeCheckMySolution(String input) {
+        /*Base case: If input length is 1, return true.*/
+        if (input.length() < 2) {
+            return true;
+        }
+
+        /*Recursive case: Check if the first and last characters match and recurse with the rest of the string*/
+        return input.charAt(0) == input.charAt(input.length() -1)
+                && palindromeCheckMySolution(input.substring(1, input.length() - 1));
+    }
+
+    public boolean palindromeCheckAuthorsSolution(String input) {
+        //Base case: return true if input is empty or has a length of 1
+        if(input.isEmpty() || input.length() == 1){
+            return true;
+        }
+
+        //Check if the first and last characters match.
+        //If yes, recursively call the function after removing the first and last characters
+        if (input.charAt(0) == input.charAt(input.length() - 1)) {
+            return palindromeCheckAuthorsSolution(input.substring(1, input.length() -1));
+        }
+
+        //return false if both of the above conditions are not met
+        return false;
+    }
 }
