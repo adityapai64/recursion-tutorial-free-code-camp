05e906a61c84b2f5a4cc64ab82d27a2bb0f6e911
Aditya Pai <aditya.pai@viada.de>
2025-01-13 17:27:02 +0100

    Divide and Conquer Algorithms: Merge Sort

--- src/DivideAndConquerAlgorithms.java
+++ src/DivideAndConquerAlgorithms.java
@@ -37,4 +37,43 @@ public class DivideAndConquerAlgorithms {
         memo.put(input, fibonacciOptimised(input - 1, memo) + fibonacciOptimised(input - 2, memo));
         return memo.get(input);
     }
+
+    public int[] mergeSort(int[] arrayToSort, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(arrayToSort, start, mid);
+            mergeSort(arrayToSort, mid + 1, end);
+        }
+        return merge(arrayToSort, start, (start + end) / 2, end);
+    }
+
+    private int[] merge(int[] intArray, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+
+        int i = start, j = mid + 1, k = 0;
+
+        //While both sub arrays have values, compare and merge them in sorted order
+        while (i <= mid && j <= end) {
+            if (intArray[i] <= intArray[j]) {
+                temp[k++] = intArray[i++];
+            } else {
+                temp[k++] = intArray[j++];
+            }
+        }
+
+        //Add the rest of the values from the left sub-array into the result.
+        while (i <= mid){
+            temp[k++] = intArray[i++];
+        }
+        //Add the rest of the values from the right sub-array into the result.
+        while (j <= end) {
+            temp[k++] = intArray[j++];
+        }
+
+        //Copy the sorted array back into the original array
+        for (i = start; i <= end; i++) {
+            intArray[i] = temp[i - start];
+        }
+        return temp;
+    }
 }
